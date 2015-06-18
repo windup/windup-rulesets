7a629e645052740c42d18df4b504409c52051c29
Matej Briskar <mbriskar@redhat.com>
2015-06-18 12:18:19 +0200

    Add new rule for InitialContext initialization

---
+++ rules-reviewed/jboss-eap/weblogic/tests/data/JNDITest.java
@@ -0,0 +1,21 @@
+// Contains code that should match for the following tests:
+//
+// weblogic-01300-test
+//
+import java.util.Hashtable;
+import javax.naming.InitialContext;
+
+class JNDITest 
+{
+    public long schedule(long time)
+    {
+	Hashtable<String, String> env = new Hashtable<String, String>();
+	env.put( "provider_url", "t3://localhost:7001");
+        InitialContext context = new InitialContext(env);
+    }
+
+    public void trigger()
+    {
+        System.out.println("trigger pulled");
+    }
+}
