e919aea234621ee22995657203f6816c8dbe7251
Marek Novotný <hotmana76@gmail.com>
2015-08-05 17:01:01 +0200

    #105 QA - SonicESB to Camel

---
+++ rules-reviewed/fuse/sonicesb/tests/data/ExampleSonicService2.java
@@ -0,0 +1,25 @@
+package com.example;
+
+import com.sonicsw.xq.XQServiceContext;
+import com.sonicsw.xq.XQInitContext;
+import com.sonicsw.xq.XQParameters;
+import com.sonicsw.xq.XQParameterInfo;
+import com.sonicsw.xq.XQMessage;
+import com.sonicsw.xq.XQPart;
+import com.sonicsw.xq.XQLog;
+import com.sonicsw.xq.XQServiceException;
+import com.sonicsw.xq.XQEnvelope;
+
+import com.sonicsw.xq.XQService;
+import com.sonicsw.xq.XQAddressFactory;
+
+/**
+ * This is a snippet of java code for testing Sonic ESB windup rules.
+ * This is not real code!
+ * 
+ * @author mnovotny
+ *
+ */
+public interface ExampleSonicService extends XQService {
+
+}
