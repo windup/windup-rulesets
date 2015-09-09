50e235eae3fab4bd728fe39979841b214d60f2e9
Matej Briskar <mbriskar@redhat.com>
2015-09-09 13:50:35 +0200

    WINDUPRULE-73: Refactoring of environment-dependent-calls

--- rules-reviewed/jboss-eap/uncategorized/tests/data/MyClass.java
+++ rules-reviewed/jboss-eap/uncategorized/tests/data/MyClass.java
@@ -1,9 +1,11 @@
 package environment.dependent;
 
-import javax.naming.InitialContext;
 import javax.management.ObjectName;
 import javax.management.remote.JMXServiceURL;
 import javax.management.remote.JMXConnectorFactory;
+import java.util.Hashtable;
+import javax.naming.InitialContext;
+import java.util.Properties;
 
 public class MyClass{
    public MyClass(){
@@ -12,7 +14,10 @@ public class MyClass{
    public void myMethod() {
 	Class.forName("environment.dependent.MyClass");
         Class.forName("environment.dependent.OtherClass");
-        InitialContext context = new InitialContext();
+        Properties environment = new Properties();
+	environment.put(Context.INITIAL_CONTEXT_FACTORY, "weblogic.jndi.WLInitialContextFactory");
+	environment.put(Context.PROVIDER_URL, "t3://localhost:7001");
+	InitialContext context = new InitialContext(environment);
         context.lookup("exampleString");
         ObjectName objectName = new ObjectName("someObjectName");
         JMXServiceURL url = new JMXServiceURL("Some service URL");
