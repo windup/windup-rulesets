8ec39c480c98597d7360eae51541facf2eb0c3ad
Marek Novotný <hotmana76@gmail.com>
2016-07-08 18:22:13 +0200

    taiga tasks 179_180 - reviewed and added rules for migration path from EAP5->7 (#166)

---
+++ rules-reviewed/eap7/eap5/tests/data/java/JaxRpcGenericHandlerTest.java
@@ -0,0 +1,35 @@
+// Contains code that should match for the following tests:
+//
+// jaxrpc-00000-test
+//
+
+import java.util.Map;
+import javax.xml.rpc.handler.Handler;
+import javax.xml.rpc.handler.GenericHandler;
+import javax.xml.rpc.handler.HandlerInfo;
+import javax.xml.rpc.handler.MessageContext;
+import javax.xml.rpc.handler.soap.SOAPMessageContext;
+
+public class JaxRpcGenericHandlerTest extends GenericHandler
+{
+    protected HandlerInfo info = null;
+
+    public void init(HandlerInfo arg)
+    {
+        info = arg;
+    }
+
+    public void destroy()
+    {
+    }
+
+    public boolean handleRequest(MessageContext context)
+    {
+        return true;
+    }
+
+    public boolean handleResponse(MessageContext context)
+    {
+        return true;
+    }
+}
