9a0cf0c2e6433858fa86e5295611c0746c5206e4
Marek Novotný <hotmana76@gmail.com>
2016-10-07 22:25:10 +0200

    added conversion script to CSV

--- scripts/inventory/inventory_to_csv.groovy
+++ scripts/inventory/inventory_to_csv.groovy
@@ -1,89 +1,67 @@
 // This script requires that you install Groovy. 
 // It creates a rules report CSV file.
 // Run it from the root of the windup-rulesets directory:
-//     groovy scripts/bigxml_to_csv.groovy
+//     groovy scripts/inventory/inventory_to_csv.groovy
 @Grab(group = 'net.sf.opencsv', module = 'opencsv', version = '2.3')
 import au.com.bytecode.opencsv.*
 
-def rules = new XmlSlurper().parse('scripts/output-data/all_rules.xml');
+def rules = new XmlSlurper().parse('all_rules.xml');
 
-maxLinkCount = 0;
-maxContentsCount = 0;
-
-rules.rule.each { rule ->
-    linkCount = rule.column.findAll { it.@name == "link" }.size();
-    if (linkCount > maxLinkCount)
-        maxLinkCount = linkCount;
-}
+maxLocationCount = 0;
 
 rules.rule.each { rule ->
-    titleCount = rule.column.findAll { it.@name == "title" }.size();
-    if (titleCount > maxContentsCount)
-        maxContentsCount = titleCount;
+    locationCount = rule.column.findAll { it.@name == "location" }.size();
+    if (locationCount > maxLocationCount)
+        maxLocationCount = locationCount;
 }
 
-//println("Max Links: " + maxLinkCount);
-//println("Max Contents: " + maxContentsCount);
-
-
-// make a List of String[]'s
+// make the header
 List header = new ArrayList();
-header.add("Rule ID");
-for (int i = 0; i < maxLinkCount; i++)
-    header.add("Link");
+header.addAll("Rule ID", "Condition Type", "Condition xml");
 
-for (int i = 0; i < maxContentsCount; i++) {
-    header.add("Title");
-    header.add("Body");
-}
+for (int i = 0; i < maxLocationCount; i++)
+    header.add("Location");
+
+header.addAll("Classification", "Hint Title", "Hint");
+
 
 Writer writer = new StringWriter()
 // make a CSV writer that writes to a string
 def w = new CSVWriter(writer)
-//w.writeNext((String[]) ['Some Things', 'Some Stuff', 'Other Junk'])
 w.writeNext(header.toArray(new String[0]));
 
-rules.rule.each { rule ->
+for (rule in rules.rule) {
     List columns = new ArrayList();
 
-    columns.add(rule.column.find {it.@name = 'id'}.toString());
+    columns.add(rule.column.find { it.@name == "id" }.toString());
+    columns.add(rule.column.find { it.@name == "condition" }.toString());
 
-    rule.column.findAll {it.@name == 'link'}.each{columns.add(it.toString())};
+    def xml_condition = rule.column.find { it.@name == "condition-content" };
+    String condition_content;
+    xml_condition.children().each { condition_content = it.name() + it.attributes().toString()}
+    columns.add(condition_content);
 
-    def linkCount = rule.column.findAll {it.@name == 'link'}.size();
-    if (linkCount < maxLinkCount) {
-        for (def i = linkCount; i < maxLinkCount; i++)
-            columns.add("");
+    // adding all locations
+    locationCount = rule.column.findAll { it.@name == "location" }.size();
+    locations = rule.column.findAll {it.@name == "location" }
+    locations.each { location ->
+        columns.add(location.text());
     }
 
-    def titleCount = rule.column.findAll { it.@name == "title"}.size();
-    rule.column.findAll { it.@name == "title" || it.@name == "contents"}.each{
-        columns.add(it.toString());
-    };
-
-    if (titleCount < maxContentsCount) {
-        for (def i = titleCount; i < maxContentsCount; i++)
-            columns.add("");
+    // add max count of header columns for locations
+    if (maxLocationCount > locationCount) {
+       for (int i = locationCount - 1; i < maxLocationCount; i++)
+           columns.add("");
     }
+
+    columns.add(rule.column.find { it.@name == "classification" }.toString());
+    columns.add(rule.column.find { it.@name == "hint-title" }.toString());
+    columns.add(rule.column.find { it.@name == "hint-message" }.toString());
 
     String[] row = columns.toArray(new String[0]);
     w.writeNext(row);
 }
 
-//for each booking
-//bookings.each { booking ->
-    // munge the data into a String[]
-    //String[] line = booking
-    //w.writeNext(line)
-
-    // if more rooms for the booking (e.g. array within current list)
-    //if (line[3] == '97') {
-        //line = ['Another Name', '', '', '']
-        //w.writeNext(line)
-        //line = ['Yet Another Name', '', '', '']
-        //w.writeNext(line)
-    //}
-//}
 writer.close()
 
 println writer.toString()
