81836ce050841c44bd1729c3c604f073beb8d5d7
mrizzi <mrizzi@redhat.com>
2017-03-13 12:45:45 +0100

    Cloud-readiness rules changed after review

---
+++ rules-reviewed/openshift/tests/data/Logging.java
@@ -0,0 +1,23 @@
+import org.apache.logging.log4j.core.appender.FileAppender;
+import org.apache.log4j.RollingFileAppender;
+import org.apache.log4j.DailyRollingFileAppender;
+import java.util.logging.FileHandler;
+import java.util.logging.SocketHandler;
+
+public class Logging {
+	
+	public class MyFileAppender extends FileAppender{}
+	public class MyRollingFileAppender extends RollingFileAppender{}
+	public class MyDailyRollingFileAppender extends DailyRollingFileAppender{}
+	public class MyFileHandler extends FileHandler{}
+	public class MySocketHandler extends SocketHandler {}
+	
+	public static void main(String argv[]) {
+		FileAppender fa = new FileAppender();
+		RollingFileAppender rfa = new RollingFileAppender();
+		DailyRollingFileAppender drfa = new DailyRollingFileAppender();
+		FileHandler fh = new FileHandler();
+		SocketHandler sh = new SocketHandler("host", 1);
+	}
+
+}
