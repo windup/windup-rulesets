137c1b238de97443f67029391d727bdfdf1612da
Marco Rizzi <mrizzi@users.noreply.github.com>
2017-09-01 17:26:34 +0200

    WINDUPRULE-274: enhanced Windows path identification and bug fix (#261)
    * WINDUPRULE-274: enhanced Windows path identification and bug fix
    
    * WINDUPRULE-274: better Windows path identification pattern

--- rules-reviewed/openshift/tests/data/LocalFileUsage.java
+++ rules-reviewed/openshift/tests/data/LocalFileUsage.java
@@ -64,6 +64,9 @@ public class LocalFileUsage {
 	    // the next 2 lines test that rule 'local-storage-00002' is only fired from the 2nd line
 	    URL url1 = new URL("url");
 	    URL url2 = new URL("file://");
+	    new File("D:/file.ext");
+		doStuff(null, "c:\foo");
+		String[] paths = new String[]{"C:/s","D:/e","E:/f"};
 	} catch (Exception e) {
 	    e.printStackTrace();
 	}
