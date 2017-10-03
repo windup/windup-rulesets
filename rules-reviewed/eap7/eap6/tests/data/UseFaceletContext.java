aea4fb7d1ff4735261dbeef0c64c28381b9e8502
Marek Novotný <hotmana76@gmail.com>
2017-10-03 14:58:20 +0200

    WINDUPRULE-303: adding https URL CN check change in system property

--- rules-reviewed/eap7/eap6/tests/data/UseFaceletContext.java
+++ rules-reviewed/eap7/eap6/tests/data/UseFaceletContext.java
@@ -4,7 +4,11 @@ import javax.faces.context.FacesContext;
 import javax.faces.view.facelets.FaceletContext;
 
 public class UseFaceletContext {
+    public static final String property = "org.jboss.security.ignoreHttpsHost";
+    
     public static void main(String[] args) {
         System.out.println(FacesContext.getCurrentInstance().getAttributes().get(FaceletContext.FACELET_CONTEXT_KEY));
+        
+        System.setProperty(property, "true");
     }
 }
