aec008a1c11919ee5eae8010a71db7855afebbc3
Marco Rizzi <mrizzi@users.noreply.github.com>
2018-02-07 16:59:55 +0100

    WINDUPRULE-103 Missing EAP 4+5 rules (#302) (#307)
    * WINDUPRULE-103 Missing EAP 4+5 rules
    
    * WINDUPRULE-103 enhanced rule after xmlfile condition fix

--- rules-reviewed/eap6/jboss-eap5/tests/samples/java/SampleService.java
+++ rules-reviewed/eap6/jboss-eap5/tests/samples/java/SampleService.java
@@ -1,5 +1,7 @@
 import org.jboss.annotation.ejb.Service;
 import org.jboss.annotation.ejb.Management;
+import org.jboss.ejb3.annotation.Service;
+import org.jboss.ejb3.annotation.Management;
 
 /**
  *  Sample class for tests.
