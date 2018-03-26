8fc37a7fed4b4c712042cc938b2cd311f0d7bbbf
Marco Rizzi <mrizzi@users.noreply.github.com>
2018-03-26 23:17:32 +0200

    WINDUPRULE-180 WebLogic rules for EJBs (#319)

--- rules-reviewed/eap6/weblogic/tests/data-ejb/WebLogicEJBTest.java
+++ rules-reviewed/eap6/weblogic/tests/data-ejb/WebLogicEJBTest.java
@@ -1,6 +1,8 @@
 import weblogic.javaee.TransactionTimeoutSeconds;
 import weblogic.ejbgen.ActivationConfigProperties;
 import weblogic.ejbgen.ActivationConfigProperty;
+import weblogic.ejbgen.MessageDriven;
+import weblogic.ejb.GenericMessageDrivenBean;
 
 @TransactionTimeoutSeconds(value=50)
 @ActivationConfigProperties(
@@ -10,7 +12,11 @@ import weblogic.ejbgen.ActivationConfigProperty;
                 )
         }
 )
-public class MyEJB 
+@MessageDriven(ejbName = "MyMessageDrivenBean",
+        destinationJndiName = "MyMessageDrivenBeanJndiName",
+        destinationType = "javax.jms.Queue")
+public class MyMessageDrivenBean 
+        extends GenericMessageDrivenBean
 {
     private String property;
 }
