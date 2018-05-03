3b8f8e71da0e3a0a5a1a435376dfab4efc547b87
Jess Sightler <jesse.sightler@gmail.com>
2018-05-03 17:28:19 -0400

    WINDUPRULE-19: JBoss WS Package Relocations

---
+++ rules-reviewed/eap7/eap5/tests/data/java/JBossWSSampleData.java
@@ -0,0 +1,17 @@
+import org.jboss.wsf.spi.annotation.*;
+import org.jboss.wsf.spi.binding.*;
+import org.jboss.wsf.spi.management.recording.*;
+import org.jboss.wsf.spi.tools.*;
+import org.jboss.wsf.spi.tools.ant.*;
+import org.jboss.wsf.spi.tools.cmd.*;
+import org.jboss.wsf.spi.util.ServiceLoader;
+import org.jboss.wsf.common.*;
+import org.jboss.wsf.common.handler.*;
+import org.jboss.wsf.common.addressing.*;
+import org.jboss.wsf.common.DOMUtils;
+import org.jboss.ws.annotation.EndpointConfig;
+
+
+public class JBossWSSampleData {
+
+}
