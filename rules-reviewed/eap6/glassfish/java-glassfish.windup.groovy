69a6f30dc5e213b5a2f73baeb8b4307363b3724d
Jesse Sightler <jesse.sightler@gmail.com>
2018-06-05 13:47:45 -0400

    WINDUPRULE-331: Fixed the implements handling and also added the case where the messageListenerInterface is specified (#332)

--- rules-reviewed/eap6/glassfish/java-glassfish.windup.groovy
+++ rules-reviewed/eap6/glassfish/java-glassfish.windup.groovy
@@ -1,6 +1,8 @@
-import org.jboss.windup.config.GraphRewrite;
+import org.jboss.windup.config.GraphRewrite
+import org.jboss.windup.config.operation.Iteration;
 import org.jboss.windup.config.parameters.ParameterizedIterationOperation;
-import org.jboss.windup.config.metadata.TechnologyReference;
+import org.jboss.windup.config.metadata.TechnologyReference
+import org.jboss.windup.rules.apps.java.scan.ast.annotations.JavaAnnotationTypeReferenceModel;
 import org.ocpsoft.rewrite.context.EvaluationContext;
 import org.ocpsoft.rewrite.param.ParameterStore;
 
@@ -16,36 +18,45 @@ ruleSet("java-glassfish-groovy")
 .addTargetTechnology(new TechnologyReference("eap", "[6,)"))
 .addRule()
     .when(
-        JavaClass.references("javax.ejb.MessageDriven").at(TypeReferenceLocation.ANNOTATION)
+        JavaClass.references("javax.jms.MessageListener").at(TypeReferenceLocation.IMPLEMENTS_TYPE).as("discard")
+        .or(JavaClass.references("javax.ejb.MessageDriven").at(TypeReferenceLocation.ANNOTATION).as("default"))
     )
     .perform(
-        new ParameterizedIterationOperation<JavaTypeReferenceModel> () {
-            Hint hint = Hint
-                .titled("MessageListener implementation is required")
-                .withText("In GlassFish, it is not always required to implement a MessageListener interface. However, with JBoss EAP 6+ it is required to implement `javax.ejb.MessageListener` in the case of a JMS MessageListener.")
-                .with(Link.to("Java EE 6 Tutorial", "https://docs.oracle.com/javaee/6/tutorial/doc/bnbpo.html"))
-                .withEffort(1);
+        Iteration.over("default").as(Iteration.DEFAULT_SINGLE_VARIABLE_STRING)
+        .perform(
+            new ParameterizedIterationOperation<JavaTypeReferenceModel> () {
+                Hint hint = Hint
+                    .titled("MDB - Implementation of MessageListener interface is required")
+                    .withText("With JBoss EAP 6+ it is required to implement `javax.jms.MessageListener` or to specify the `messageListenerInterface` attribute in the `@MessageDriven` annotation in the case of a JMS MessageListener.")
+                    .with(Link.to("MessageDriven Javadoc JavaEE 6", "https://docs.oracle.com/javaee/6/api/javax/ejb/MessageDriven.html"))
+                    .with(Link.to("MessageDriven Javadoc JavaEE 7", "https://docs.oracle.com/javaee/7/api/javax/ejb/MessageDriven.html"))
+                    .with(Link.to("Java EE 6 Tutorial", "https://docs.oracle.com/javaee/6/tutorial/doc/bnbpo.html"))
+                    .withEffort(1);
 
-            public Set<String> getRequiredParameterNames() {
-                return hint.getRequiredParameterNames();
-            }
-
-            public void setParameterStore(ParameterStore store) {
-                hint.setParameterStore(store);
-            }
-
-            public void performParameterized(final GraphRewrite event, final EvaluationContext context, final JavaTypeReferenceModel locationModel) {
-                boolean implementsMessageListener = false;
-                for (FileLocationModel otherLocation : locationModel.getFile().getAllTypeReferences()) {
-                    if (!(otherLocation instanceof JavaTypeReferenceModel))
-                        continue;
-
-                    if (otherLocation.getResolvedSourceSnippit() != null && otherLocation.getResolvedSourceSnippit().equals("javax.jms.MessageListener"))
-                        implementsMessageListener = true;
+                public Set<String> getRequiredParameterNames() {
+                    return hint.getRequiredParameterNames();
                 }
-                if (!implementsMessageListener)
-                    hint.perform(event, context)
+
+                public void setParameterStore(ParameterStore store) {
+                    hint.setParameterStore(store);
+                }
+
+                public void performParameterized(final GraphRewrite event, final EvaluationContext context, final JavaTypeReferenceModel locationModel) {
+                    boolean implementsMessageListener = false;
+                    boolean hasMessageListenerAttribute = ((JavaAnnotationTypeReferenceModel)locationModel).annotationValues.get("messageListenerInterface") != null;
+
+                    for (FileLocationModel otherLocation : locationModel.getFile().getAllTypeReferences()) {
+                        if (!(otherLocation instanceof JavaTypeReferenceModel))
+                            continue;
+
+                        if (otherLocation.resolvedSourceSnippit != null && otherLocation.resolvedSourceSnippit.equals("javax.jms.MessageListener") && otherLocation.referenceLocation == TypeReferenceLocation.IMPLEMENTS_TYPE)
+                            implementsMessageListener = true;
+                    }
+
+                    if (!hasMessageListenerAttribute && !implementsMessageListener)
+                        hint.perform(event, context)
+                }
             }
-        }
+        )
     )
     .withId("java-glassfish-groovy-01000")
