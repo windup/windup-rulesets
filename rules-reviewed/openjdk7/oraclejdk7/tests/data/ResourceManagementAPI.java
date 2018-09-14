d02e079e5b66a16ac1952eba2ab49bb87fb1afd5
Janine Eichler <jeichler@users.noreply.github.com>
2018-09-14 15:23:53 +0200

    WINDUPRULE-353 Set of rules to cover potential issues when migrating from oracle to openjdk (#342)
    * add rules for migrating from oracle to openjdk
    
    * add KeyAgreement.getInstance to elliptic curves rule
    
    * - use iterabe-filter in all openjdk tests
    - change folder structure for openjdk according to current pattern
    - format links for openjdk crypto rule

---
+++ rules-reviewed/openjdk7/oraclejdk7/tests/data/ResourceManagementAPI.java
@@ -0,0 +1,11 @@
+package com.acme;
+
+import jdk.management.resource.*;
+
+public class ResourceManagementAPI {
+
+    public static void main(String[] args) {
+        System.println("Hello world");
+    }
+
+}
