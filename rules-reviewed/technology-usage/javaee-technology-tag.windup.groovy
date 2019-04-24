b91a84c95507d8e91d9d3b83c7a5b888978e9368
Jonathan Vila <jonathan.vila@gmail.com>
2019-04-24 19:33:50 +0200

    Windup 2360 add technology tags (#374)
    * windup-2360 created Groovy Rule and test for all the technologies
    
    * windup-2360 fixed patterns for the tests on technology-tag
    
    * windup-2360 [PR] changed tests ids to be unique

---
+++ rules-reviewed/technology-usage/javaee-technology-tag.windup.groovy
@@ -0,0 +1,31 @@
+import org.jboss.windup.config.GraphRewrite
+import org.jboss.windup.config.operation.iteration.AbstractIterationOperation
+import org.jboss.windup.config.phase.DependentPhase
+import org.jboss.windup.config.phase.PostMigrationRulesPhase
+import org.jboss.windup.config.phase.PreReportGenerationPhase
+import org.jboss.windup.config.query.Query
+import org.jboss.windup.reporting.model.TechnologyTagLevel
+import org.jboss.windup.reporting.model.TechnologyUsageStatisticsModel
+import org.jboss.windup.reporting.service.TechnologyTagService
+import org.ocpsoft.rewrite.context.EvaluationContext
+
+List<String> technologies = Arrays.asList("EJB", "Entity Bean", "JAX-RS", "JAX-WS", "JCA", "JDBC datasources", "JDBC XA datasources" , 
+        "JMS Connection Factory", "JMS Queue", "JMS Topic", "JPA entities", "JPA named queries", "JSF Page", "JSP Page", "JTA" , 
+        "Message (MDB)", "Persistence units", "RMI", "Stateful (SFSB)", "Stateless (SLSB)")
+
+ruleSet("javaee-technology-tag")
+    .setPhase(DependentPhase.class)
+    .addExecuteAfter(PostMigrationRulesPhase.class)
+    .addExecuteBefore(PreReportGenerationPhase.class)
+    .addRule()
+    .when(Query.fromType(TechnologyUsageStatisticsModel.class))
+    .perform(new AbstractIterationOperation<TechnologyUsageStatisticsModel>() {
+        void perform(GraphRewrite event, EvaluationContext context, TechnologyUsageStatisticsModel payload) {
+            TechnologyTagService technologyTagService = new TechnologyTagService(event.getGraphContext())
+            if (technologies.contains(payload.getName().trim())) {
+                technologyTagService.addTagToFileModel(payload.getProjectModel().getRootFileModel(), payload.getName(), TechnologyTagLevel.INFORMATIONAL)
+            }
+        }
+    })
+    .withId("javaee-technology-tag-000100")
+
