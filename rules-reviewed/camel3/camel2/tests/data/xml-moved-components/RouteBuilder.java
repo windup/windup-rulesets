9a600e79ccfd7f473c9ff141827c332f1cf154f6
mmelko <mmelko@redhat.com>
2020-01-28 09:12:55 +0100

    [WINDUPRULE-435] Camel-dataformat moved out of camel-core

--- rules-reviewed/camel3/camel2/tests/data/xml-moved-components/RouteBuilder.java
+++ rules-reviewed/camel3/camel2/tests/data/xml-moved-components/RouteBuilder.java
@@ -17,6 +17,7 @@ public class MyRouteBuilder extends RouteBuilder {
             .to("mock:result");
 
         from("ref:endpoint1")
+            .marshal().zipDeflater()
             .to("browse:orderReceived")
             .to("controlbus:route?routeId=mainRoute&action=stop&async=true")
             .to("language:simple:classpath:org/apache/camel/component/language/mysimplescript.txt")
@@ -24,7 +25,9 @@ public class MyRouteBuilder extends RouteBuilder {
 
         from("dataset:foo")
             .to("direct-vm:bar");
-            .from("scheduler://foo?delay=60s")
+            .unmarshal().zipDeflater().process(new UnZippedMessageProcessor());
+
+        from("scheduler://foo?delay=60s")
             .to("seda:next")
             .to("stub:smtp://somehost.foo.com?user=windup");
 
