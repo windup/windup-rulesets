3cb85e29339bf97308c2b60639910fc37d334b18
Jonathan Vila <jvilalop@redhat.com>
2020-04-29 09:37:26 +0200

    Winduprule 476 spring metrics quarkus (#429)
    * WINDUPRULE-476 added demo file
    
    * WINDUPRULE-476 added rule and test rule
    
    * WINDUPRULE-476 improved text of the rule
    
    * WINDUPRULE-476 added trailing spaces to rule message
    
    * WINDUPRULE-476 Moved to a Target->Source folder structure
    
    * WINDUPRULE-476 added runtime label for Quarkus
    
    * WINDUPRULE-476 added Technology Tag & Technology Usage for SB micrometer
    
    * WINDUPRULE-476 added dependencies on rules and jar artifact to test the technology tag
    
    * WINDUPRULE-476 redefined effort SP
    
    * WINDUPRULE-476 extracted source from test data artifact
    removed dependency between rules
    
    * WINDUPRULE-476 added quickfixes for the metrics rule
    
    * WINDUPRULE-476 removed runtime label and fixed quickfix
    
    * WINDUPRULE-476 fixed technology tag for Micrometer
    
    * winduprule-476 fixed rules ids to not collide with the other quarkus ones
    
    * winduprule-476 replace text on rule test
    removed quickfixes
    added extra rule for Timed annotation
    
    * winduprule-476 added annotation to test data
    changed effort for annotations replacement
    added not-has-hint to global search
    
    * winduprule-476 changed effort on the artifact rule
    changed Javadoc links to the latest version of the artifact
    
    * WINDUPRULE-476  [PR] fixed trailing spaces
    
    * WINCUPRULE-476 remove blank line
    
    * winduprule-476 [PR] fix typos

---
+++ rules-reviewed/quarkus/springboot/tests/data/springboot-metrics/SampleBean.java
@@ -0,0 +1,26 @@
+package com.example.demometricsactuator;
+
+import org.springframework.stereotype.Component;
+
+import io.micrometer.core.annotation.Timed;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+
+/**
+ * SampleBean
+ */
+@Component
+public class SampleBean {
+    private final Counter counter;
+
+    public SampleBean(MeterRegistry registry) {
+        this.counter = registry.counter("received.messages");
+    }
+
+    @Timed("handleMessage")
+    public void handleMessage(String message) {
+        this.counter.increment();
+        // handle message implementation
+    }
+    
+}
---
+++ rules-reviewed/quarkus/springboot/tests/data/springboot-metrics/SampleService.java
@@ -0,0 +1,27 @@
+package com.example.demometricsactuator;
+
+import org.springframework.stereotype.Service;
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.annotation.Counted;
+import io.micrometer.core.annotation.Timed;
+
+/**
+ * SampleService
+ */
+@Service
+public class SampleService {
+    private final Counter counter;
+
+    @Timed("timer")
+    @Counted("counter")
+    public void execute() {
+        counter.increment();
+    }
+
+	public SampleService(MeterRegistry meterRegistry) {
+        counter = meterRegistry.counter("name", "");
+    }
+
+}
