62b5e992652eb28be7cc9e0fc4696cd6d2d1dc82
Marco Rizzi <mrizzi@users.noreply.github.com>
2020-10-14 17:52:43 +0200

    WINDUPRULE-627 Changed rule pattern  (#6)
    * WINDUPRULE-667 Wider groupId condition to catch more Spring artifacts (#506)
    
    * WINDUPRULE-667 Wider groupId condition to catch more Spring artifacts
    
    * WINDUPRULE-667 Missed test JAR in previous commit
    
    * WINDUPRULE-624: add rule condition for source mode analysis (#497)
    
    * WINDUPRULE-670: spring aop source analysis rule (#505)
    
    * WINDUPRULE-670: spring aop source analysis rule
    
    * WINDUPRULE-670: add aop artifact to SHA1 hashes
    
    * WINDUPRULE-670: add spring-boot-starter-aop artifact to rule
    
    * WINDUPRULE-627 Changed rule pattern
    
    Co-authored-by: Mark Brophy <36955467+m-brophy@users.noreply.github.com>

--- rules-reviewed/quarkus/springboot/springboot-flo-to-quarkus.windup.groovy
+++ rules-reviewed/quarkus/springboot/springboot-flo-to-quarkus.windup.groovy
@@ -1,21 +1,11 @@
 package quarkus.springboot
 
 import org.jboss.windup.config.metadata.TechnologyReference
-import org.jboss.windup.config.operation.Iteration
-import org.jboss.windup.config.phase.PostMigrationRulesPhase
-import org.jboss.windup.project.condition.Artifact
-import org.jboss.windup.project.condition.Project
-import org.jboss.windup.rules.files.condition.File
-import org.jboss.windup.rules.files.condition.FileContent
 import org.jboss.windup.reporting.category.IssueCategory
 import org.jboss.windup.reporting.category.IssueCategoryRegistry
-import org.jboss.windup.reporting.config.HasHint
 import org.jboss.windup.reporting.config.Hint
 import org.jboss.windup.reporting.config.Link
-import org.jboss.windup.rules.apps.java.condition.SourceMode
-import org.jboss.windup.rules.files.condition.FileContentFileName
-import org.ocpsoft.rewrite.config.And
-import org.ocpsoft.rewrite.config.Not
+import org.jboss.windup.rules.files.condition.FileContent
 
 final IssueCategory potentialIssueCategory = new IssueCategoryRegistry().getByID(IssueCategoryRegistry.POTENTIAL)
 final Link guideLink = Link.to("Quarkus - Guides", "https://quarkus.io/guides/")
@@ -23,23 +13,21 @@ final Link guideLink = Link.to("Quarkus - Guides", "https://quarkus.io/guides/")
 ruleSet("springboot-flo-to-quarkus-groovy")
         .addSourceTechnology(new TechnologyReference("springboot", null))
         .addTargetTechnology(new TechnologyReference("quarkus", null))
-        .setPhase(PostMigrationRulesPhase.class)
         .addRule()
         .when(
-            //FileContent.matches("{.*}{flo-editor}{.*}").andNot(File.inFileNamed("{*}.(java|properties|tag|xml|txt)"))
-            FileContent.matches("{.*}").and(File.inFileNamed("springboot-flo-test.component.html"))
+            FileContent.matches("{prefixes}flo-editor").inFileNamed("{*}.{extensions}")
         )
         .perform(
-        Iteration.over("file")
-                .perform(((Hint) Hint.titled("Spring Flo is not supported by Quarkus")
-                .withText("""
-                                Spring Flo Javascript library has been detected.
-
-                        This is used in conjunction with Spring Cloud Data Flow which is not supported in Quarkus
-                                """)
-                .withIssueCategory(mandatoryIssueCategory)
-                .with(guideLink)
-                .withEffort(13)))
-                .endIteration()
+            ((Hint) Hint.titled("Spring Flo is not supported by Quarkus")
+            .withText("""
+                            Spring Flo Javascript library has been detected.  
+                            This is used in conjunction with Spring Cloud Data Flow which is not supported in Quarkus  
+                            The link below will take to Quarkus Guides where you can check if there are alternatives for your use case.
+                            """)
+            .withIssueCategory(potentialIssueCategory)
+            .with(guideLink)
+            .withEffort(13))
         )
+        .where("prefixes").matches("(<|')")
+        .where("extensions").matches("(html|ts)")
         .withId("springboot-flo-to-quarkus-groovy-00000")
