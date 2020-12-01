34db66eee119c8f78aa9925e1b1cf2676cf582eb
mrizzi <mrizzi@redhat.com>
2020-12-01 19:45:39 +0100

    WINDUPRULE-676 Quarkus quarkus-narayana-jta extension rule

--- rules-generated/quarkus/narayana/tests/data/SampleClass.java
+++ rules-generated/quarkus/narayana/tests/data/SampleClass.java
@@ -1,3 +1,7 @@
+import com.arjuna.ats.arjuna.WhateverV3;
+import com.arjuna.ats.arjuna.*;
+import com.arjuna.ats.arjuna.another.should.not.trigger.the.rule.*;
+import com.arjuna.ats.arjuna.another.as.Well;
 import org.jboss.stm.WhateverV3;
 import org.jboss.stm.*;
 import org.jboss.stm.another.should.not.trigger.the.rule.*;
