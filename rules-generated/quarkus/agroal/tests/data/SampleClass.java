0aaa7d473e87fbafb7245f59403dcea3ede580a3
mrizzi <mrizzi@redhat.com>
2020-12-03 17:02:22 +0100

    WINDUPRULE-676 Quarkus quarkus-agroal extension rule

---
+++ rules-generated/quarkus/agroal/tests/data/SampleClass.java
@@ -0,0 +1,8 @@
+import io.agroal.api.WhateverV3;
+import io.agroal.api.*;
+import io.agroal.api.another.should.not.trigger.the.rule.*;
+import io.agroal.api.another.as.Well;
+
+public class SampleClass {
+    public static void main(String[] args) {}
+}
