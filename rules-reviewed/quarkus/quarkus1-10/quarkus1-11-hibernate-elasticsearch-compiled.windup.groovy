d58c0733820f0f68d7a08eb238dceaa8d96f4e38
Marco Rizzi <mrizzi@users.noreply.github.com>
2021-02-04 19:50:52 +0100

    WINDUPRULE-710 Divived Groovy rule in two different rules (#7)
    * WINDUPRULE-708 Quarkus Release 1.11 - Jackson (#523)
    
    * WINDUPRULE-708 Quarkus Release 1.11 - Jackson
    
    * WINDUPRULE-708 - rename tests to be consistent with rules
    
    * WINDUPRULE-708 changed to quarkus1 target so new quarkus1 folder under rules-reviewed
    
    Co-authored-by: Phil Cattanach <pcattana@pcattana.remote.csb>
    
    * WINDUPRULE-710 Divived Groovy rule in two different rules
    
    Co-authored-by: PhilipCattanach <31246010+PhilipCattanach@users.noreply.github.com>
    Co-authored-by: Phil Cattanach <pcattana@pcattana.remote.csb>

--- rules-reviewed/quarkus/quarkus1-10/quarkus1-11-hibernate-elasticsearch-compiled.windup.groovy
+++ rules-reviewed/quarkus/quarkus1-10/quarkus1-11-hibernate-elasticsearch-compiled.windup.groovy
@@ -25,13 +25,10 @@ ruleSet("quarkus1-11-hibernate-elasticsearch-compiled-groovy")
         .addTargetTechnology(new TechnologyReference("quarkus", "[11,)")) 
         .addRule()
         .when(SourceMode.isDisabled(),
-                Or.any(
-                    Query.fromType(FileModel)
-                    .withProperty(FileModel.IS_DIRECTORY, Boolean.TRUE)
-                    .withProperty(FileModel.FILE_PATH, QueryPropertyComparisonType.REGEX, ".*/io/quarkus/hibernate/search/orm/elasticsearch\$"),
-                    FileContent.matches("quarkus.hibernate-search-orm.elasticsearch.version").inFileNamed("application.properties")
-                )
-)
+                Query.fromType(FileModel)
+                .withProperty(FileModel.IS_DIRECTORY, Boolean.TRUE)
+                .withProperty(FileModel.FILE_PATH, QueryPropertyComparisonType.REGEX, ".*/io/quarkus/hibernate/search/orm/elasticsearch\$")
+        )
         .perform(new AbstractIterationOperation<FileModel>() {
             void perform(GraphRewrite event, EvaluationContext context, FileModel payload) {
                 //ensure that the dependency being searched for is present in the current application
@@ -52,15 +49,44 @@ ruleSet("quarkus1-11-hibernate-elasticsearch-compiled-groovy")
                 folderLocationModel.setLength(1)
                 folderLocationModel.setSourceSnippit("Folder Match")
                 ((Hint) Hint.titled("The default required status for Elasticsearch indexes is now yellow")
-                    .withText("""The default required status for Elasticsearch indexes is now yellow.
-                            If you have specific requirements and need to wait for indexes to be green on startup,
-                            set quarkus.hibernate-search.elasticsearch.schema-management.required-status to green.  
+                    .withText("""The default required status for Elasticsearch indexes is now `yellow`.  
+                            If you have specific requirements and need to wait for indexes to be `green` on startup,
+                            set `quarkus.hibernate-search.elasticsearch.schema-management.required-status` to `green`.  
                             Refer to the guide below for more details about how to return to the previous behaviour.""")
                     .withIssueCategory(potentialIssueCategory)
                     .with(Link.to("Quarkus - Hibernate Search Guide", "https://quarkus.io/guides/hibernate-search-orm-elasticsearch"))
-                    .with(Link.to("Quarkus - Migraton Guide 1.11", "https://github.com/quarkusio/quarkus/wiki/Migration-Guide-1.11"))
+                    .with(Link.to("Quarkus - Migration Guide 1.11", "https://github.com/quarkusio/quarkus/wiki/Migration-Guide-1.11"))
                     .withEffort(1)
                 ).performParameterized(event, context, folderLocationModel)
             }
         })
         .withId("quarkus1-11-hibernate-elasticsearch-compiled-groovy-00000")
+        .addRule()
+        .when(SourceMode.isDisabled(),
+                FileContent.matches("quarkus.hibernate-search-orm.elasticsearch.version").inFileNamed("application.properties")
+        )
+        .perform(new AbstractIterationOperation<FileLocationModel>() {
+            void perform(GraphRewrite event, EvaluationContext context, FileLocationModel payload) {
+                // ensure that the application.properties being searched for is present
+                // in the current application rather than any application in the MTA project
+                final String sourceBasePath = payload.getFile().getFilePath().replace("application.properties", "")
+                final String dependencyJarName = sourceBasePath.substring(sourceBasePath.lastIndexOf("/") + 1)
+                WindupConfigurationModel windupConfigurationModel = WindupConfigurationService.getConfigurationModel(event.getGraphContext())
+                boolean propertiesFileComesFromAnalyzedApplication = false
+                windupConfigurationModel.getInputPaths().each {
+                    if (!propertiesFileComesFromAnalyzedApplication && it.filePath.endsWith(dependencyJarName)) propertiesFileComesFromAnalyzedApplication = true
+                }
+                if (!propertiesFileComesFromAnalyzedApplication) return
+                ((Hint) Hint.titled("The default required status for Elasticsearch indexes is now yellow")
+                    .withText("""The default required status for Elasticsearch indexes is now `yellow`.  
+                            If you have specific requirements and need to wait for indexes to be `green` on startup,
+                            set `quarkus.hibernate-search.elasticsearch.schema-management.required-status` to `green`.  
+                            Refer to the guide below for more details about how to return to the previous behaviour.""")
+                    .withIssueCategory(potentialIssueCategory)
+                    .with(Link.to("Quarkus - Hibernate Search Guide", "https://quarkus.io/guides/hibernate-search-orm-elasticsearch"))
+                    .with(Link.to("Quarkus - Migration Guide 1.11", "https://github.com/quarkusio/quarkus/wiki/Migration-Guide-1.11"))
+                    .withEffort(1)
+                ).performParameterized(event, context, payload)
+            }
+        })
+        .withId("quarkus1-11-hibernate-elasticsearch-compiled-groovy-00010")
