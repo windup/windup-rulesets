4efaa5c8e52b52ff3767f5230778220f448508c6
Carlos E. Feria Vila <carlosthe19916@gmail.com>
2021-03-03 03:18:23 -0800

    WINDUPRULE-719: Quarkus Release 1.12 - HTTP (#538)
    * Add quarkus-resteasy-reactive HTTP property changes
    
    * Enhance message body
    
    * Change the version of the data pom.xml
    
    * Reduce the number of tests using <iterable-filter size="3">
    
    * Apply Marco's enhancements

---
+++ rules-reviewed/quarkus1/quarkus1-11/tests/data/resteasy-reactive/Endpoint.java
@@ -0,0 +1,19 @@
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.jboss.resteasy.reactive.MultipartForm;
+
+@Path("multipart")
+public class Endpoint {
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Path("form")
+    public String form(@MultipartForm FormData formData) {
+        // return something
+    }
+}
