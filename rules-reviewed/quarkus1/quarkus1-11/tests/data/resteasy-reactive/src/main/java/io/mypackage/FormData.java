8de74b1a52e554fc24680e81c1b2f444b8769da9
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2021-03-08 17:30:16 +0100

    Use org.jboss.resteasy.annotations.providers.multipart.MultipartForm

---
+++ rules-reviewed/quarkus1/quarkus1-11/tests/data/resteasy-reactive/src/main/java/io/mypackage/FormData.java
@@ -0,0 +1,19 @@
+package io.mypackage;
+
+import java.io.InputStream;
+
+import javax.ws.rs.FormParam;
+import javax.ws.rs.core.MediaType;
+
+import org.jboss.resteasy.annotations.providers.multipart.PartType;
+
+public class FormData {
+
+    @FormParam("file")
+    @PartType(MediaType.APPLICATION_OCTET_STREAM)
+    public InputStream file;
+
+    @FormParam("fileName")
+    @PartType(MediaType.TEXT_PLAIN)
+    public String fileName;
+}
