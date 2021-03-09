cb134afbd718ab14bbe769126e2f368b5affe652
Carlos E. Feria Vila <carlosthe19916@gmail.com>
2021-03-09 14:26:06 +0100

    WINDUPRULE-719: Quarkus Release 1.12 - HTTP - fix (#542)
    * Use org.jboss.resteasy.annotations.providers.multipart.MultipartForm
    
    * Enhanced hint's message and 'simplified' test (#2)
    
    Co-authored-by: Marco Rizzi <mrizzi@users.noreply.github.com>

--- rules-reviewed/quarkus1/quarkus1-11/tests/data/resteasy-reactive/Endpoint.java
+++ rules-reviewed/quarkus1/quarkus1-11/tests/data/resteasy-reactive/Endpoint.java
@@ -1,10 +1,12 @@
+package io.mypackage;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import org.jboss.resteasy.reactive.MultipartForm;
+import org.jboss.resteasy.annotations.providers.multipart.MultipartForm;
 
 @Path("multipart")
 public class Endpoint {
@@ -14,6 +16,6 @@ public class Endpoint {
     @Consumes(MediaType.MULTIPART_FORM_DATA)
     @Path("form")
     public String form(@MultipartForm FormData formData) {
-        // return something
+         return "something";
     }
 }
