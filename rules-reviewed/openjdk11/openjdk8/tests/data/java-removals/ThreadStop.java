11e886fe4efa8cfd8ccb456d06c1229beae065af
Andrew Hughes <gnu.andrew@redhat.com>
2021-06-16 15:39:50 +0100

    WINDUPRULE-754 java.lang.Thread.stop(java.lang.Throwable) has been removed

---
+++ rules-reviewed/openjdk11/openjdk8/tests/data/java-removals/ThreadStop.java
@@ -0,0 +1,15 @@
+public class ThreadStop {
+
+    public static void main(String[] args) {
+	Thread t1 = new Thread(new Runnable() {
+		@Override
+		public void run() {
+		    while (true) {
+			System.out.println("Test thread running.");
+		    }
+		}
+	    }, "Test Thread");
+	t1.start();
+	t1.stop(new ArrayIndexOutOfBoundsException());
+    }
+}
