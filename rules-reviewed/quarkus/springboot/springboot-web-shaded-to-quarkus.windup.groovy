04d2a7a963a8c38a281655e3b9c1be97997a2426
m-brophy <mbrophy@redhat.com>
2022-01-17 14:50:19 +0000

    WINDUPRULE-815: tidy up rule cosmetically

--- rules-reviewed/quarkus/springboot/springboot-web-shaded-to-quarkus.windup.groovy
+++ rules-reviewed/quarkus/springboot/springboot-web-shaded-to-quarkus.windup.groovy
@@ -6,10 +6,8 @@ import org.jboss.windup.config.operation.iteration.AbstractIterationOperation
 import org.jboss.windup.config.query.Query
 import org.jboss.windup.config.query.QueryPropertyComparisonType
 import org.jboss.windup.graph.model.FileLocationModel
-import org.jboss.windup.graph.model.WindupConfigurationModel
 import org.jboss.windup.graph.model.resource.FileModel
 import org.jboss.windup.graph.service.GraphService
-import org.jboss.windup.graph.service.WindupConfigurationService
 import org.jboss.windup.reporting.category.IssueCategory
 import org.jboss.windup.reporting.category.IssueCategoryRegistry
 import org.jboss.windup.reporting.config.Hint
@@ -23,15 +21,15 @@ ruleSet("springboot-web-shaded-to-quarkus")
         .addSourceTechnology(new TechnologyReference("springboot", null))
         .addTargetTechnology(new TechnologyReference("quarkus", null))
         .addRule()
+
         .when(SourceMode.isDisabled(),
                 Query.fromType(FileModel)
                         .withProperty(FileModel.IS_DIRECTORY, Boolean.TRUE)
                         .withProperty(FileModel.FILE_PATH, QueryPropertyComparisonType.REGEX, ".*/org/springframework/web\$"))
+
         .perform(new AbstractIterationOperation<FileModel>() {
             void perform(GraphRewrite event, EvaluationContext context, FileModel payload) {
-                final String sourceBasePath = payload.getFilePath().replaceAll("/org/springframework/web\$", "")
-                final String dependencyJarName = sourceBasePath.substring(sourceBasePath.lastIndexOf("/") + 1)
-                WindupConfigurationModel windupConfigurationModel = WindupConfigurationService.getConfigurationModel(event.getGraphContext())
+
                 final GraphService<FileLocationModel> fileLocationService = new GraphService<>(event.getGraphContext(), FileLocationModel.class)
                 final FileLocationModel folderLocationModel = fileLocationService.create()
                 folderLocationModel.setFile(payload)
@@ -39,6 +37,7 @@ ruleSet("springboot-web-shaded-to-quarkus")
                 folderLocationModel.setLineNumber(1)
                 folderLocationModel.setLength(1)
                 folderLocationModel.setSourceSnippit("Folder Match")
+
                 ((Hint) Hint.titled("Replace the 'spring-web' dependency with Quarkus 'quarkus-spring-web' extension")
                         .withText("""
                                     A folder path related to a package from the `org.springframework:spring-web` dependency has been found.  
