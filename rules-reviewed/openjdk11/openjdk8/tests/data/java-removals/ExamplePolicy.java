c6936a296b284192b99bfd9ffd1bcb7fd2051175
m-brophy <mbrophy@redhat.com>
2022-03-18 12:47:31 +0000

    WINDUPRULE-796: changes to rule for removed javax.security.auth.Policy

--- rules-reviewed/openjdk11/openjdk8/tests/data/java-removals/ExamplePolicy.java
+++ rules-reviewed/openjdk11/openjdk8/tests/data/java-removals/ExamplePolicy.java
@@ -1,15 +1,20 @@
 import javax.security.auth.Policy;
-public static void main(String[] args) {
 
-    CodeSource source;
+public class ExamplePolicy {
 
-    try {
-      source = new CodeSource(new URL("file:/c:/*"), (java.security.cert.Certificate[]) null);
 
-      Policy policy = Policy.getPolicy();
-      System.out.println(policy.getPermissions(source));
+    public static void main(String[] args) {
 
-    } catch (IOException e) {
-      e.printStackTrace();
+        CodeSource source;
+
+        try {
+            source = new CodeSource(new URL("file:/c:/*"), (java.security.cert.Certificate[]) null);
+
+            Policy policy = Policy.getPolicy();
+            System.out.println(policy.getPermissions(source));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-  }
+}
