e8a9612f6fee455e741c8aad0d2df1f47863b6de
mrizzi <mrizzi@redhat.com>
2022-03-28 15:47:44 +0200

    WINDUP-3317 Additional classpath JAR additions

--- rules-reviewed/openjdk11/openjdk8/tests/data/java-removals/Base64Removals.java
+++ rules-reviewed/openjdk11/openjdk8/tests/data/java-removals/Base64Removals.java
@@ -1,17 +1,19 @@
 package com.test.project;
 
-import sun.misc.BASE64Decoder;
+import sun.misc.*;
 import sun.misc.BASE64Encoder;
 
 import java.io.IOException;
 
 public class Base64Removals {
     public static void main(String[] args) {
-        BASE64Encoder base64Encoder = new BASE64Encoder();
+        BASE64Encoder base64Encoder;
+        base64Encoder = new BASE64Encoder();
         BASE64Decoder base64Decoder = new BASE64Decoder();
 
         try {
             base64Decoder.decodeBuffer("inputString");
+            base64Decoder.bytesPerAtom();
             base64Encoder.encodeBuffer(new byte[]{'a', 'b'});
         } catch (IOException e) {
             e.printStackTrace();
