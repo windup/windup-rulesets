ecde89832d7529e6103a196469cbf4575b9f9c4f
Denis Fuenzalida 🐙 <Denis.Fuenzalida@microsoft.com>
2022-06-16 18:15:03 -0700

    Check usage of filesystem APIs for EAP on App Service

---
+++ rules-reviewed/azure/eap/test/data/eap-to-azure-appservice-file-system/FileSystem.java
@@ -0,0 +1,15 @@
+import java.notio.File;
+import java.io.File;
+import java.nio.File;
+import java.notnio.File;
+import org.apache.commons.io.File;
+import org.apache.commons.notio.File;
+
+public class FileSystem {
+
+    String path = "/here/it/is";
+    String relativePath = "./here/it/is";
+    String relativePath2 = "../here/it/is";
+    String homePath = "/home/it/is";
+    String notHomePath = "/nothome/it/is";
+ }
