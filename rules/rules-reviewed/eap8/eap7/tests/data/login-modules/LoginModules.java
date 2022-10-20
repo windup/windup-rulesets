ca9a24ce0064a42fbb42e5824630ef9a9f723b2a
Juan Manuel Leflet Estrada <jleflete@redhat.com>
2022-10-20 15:50:02 +0200

    [WINDUPRULE-904] Some fixes

--- rules/rules-reviewed/eap8/eap7/tests/data/login-modules/LoginModules.java
+++ rules/rules-reviewed/eap8/eap7/tests/data/login-modules/LoginModules.java
@@ -6,7 +6,7 @@ public class JakartaCDI {
         
     }
 
-    private class C1 extends org.jboss.security.auth.spi.DeploymentRolsesLoginModule {
+    private class C1 extends org.jboss.security.auth.spi.DeploymentRolesLoginModule {
 
     }
 
