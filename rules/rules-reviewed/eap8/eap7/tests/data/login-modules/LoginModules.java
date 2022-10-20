60e86cbf83c5aed7a8d4705e425f8edb58314590
Juan Manuel Leflet Estrada <jleflete@redhat.com>
2022-10-20 12:15:27 +0200

    [WINDUPRULE-904] Add rules to detect LoginModules migration issues

---
+++ rules/rules-reviewed/eap8/eap7/tests/data/login-modules/LoginModules.java
@@ -0,0 +1,20 @@
+package com.test.project;
+
+public class JakartaCDI {
+
+    private class C1 extends org.jboss.security.auth.spi.BaseCertLoginModule {
+        
+    }
+
+    private class C1 extends org.jboss.security.auth.spi.DeploymentRolsesLoginModule {
+
+    }
+
+    private class C1 extends org.jboss.security.auth.spi.IdentityLoginModule {
+
+    }
+
+    private class C1 extends org.jboss.security.auth.spi.UserNamePasswordLoginModule {
+
+    }
+}
