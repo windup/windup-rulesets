86e3dd69459c92e7e1eae6523d21a021649de14e
Mark Brophy <36955467+m-brophy@users.noreply.github.com>
2022-12-12 10:14:57 +0000

    WINDUPRULE-893: eap8 ejb removed methods rules (#770)
    * WINDUPRULE-909: eap8 ejb removed methods rules and test
    
    * WINDUPRULE-893: fix for tests
    
    * removal of commented out test
    
    * rules not firing
    
    * alter rule method sugnature
    
    * rule method signature altered
    
    * fixing tests

---
+++ rules/rules-reviewed/eap8/eap6/tests/data/ejb/EJBContextMethodsTest.java
@@ -0,0 +1,32 @@
+package data.ejb;
+
+
+import javax.ejb.EJBContext;
+import javax.ejb.SessionContext;
+import javax.annotation.Resource;
+import java.security.Identity;
+import java.util.Properties;
+import javax.xml.rpc.handler.MessageContext;
+
+
+public class EJBContextMethodsTest  {
+    
+    @Resource
+    private EJBContext context;
+
+    // Inject the Session Context
+    @Resource
+    private SessionContext sessionCtx;
+
+    public String securityCalls(Identity role) {
+
+        try {
+            Identity caller = context.getCallerIdentity();
+            boolean inRole = context.isCallerInRole(role);
+            Properties env = context.getEnvironment();
+            MessageContext mCx = sessionCtx.getMessageContext();
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
