0fd5f191093e6c1adfe06c68cbaab1b5c96e44eb
Juan Manuel Leflet Estrada <jleflete@redhat.com>
2023-02-07 12:14:02 +0100

    [WINDUPRULE-941] Add rules for deprecated usages

--- rules/rules-reviewed/hibernate/tests/data/hibernate-search-61/HibernateSearch61TestClass.java
+++ rules/rules-reviewed/hibernate/tests/data/hibernate-search-61/HibernateSearch61TestClass.java
@@ -10,8 +10,9 @@ import org.hibernate.search.engine.backend.work.execution.spi.IndexIndexingPlan;
 import org.hibernate.search.engine.backend.work.execution.spi.IndexIndexingPlanExecutionReport;
 import org.hibernate.search.engine.common.timing.spi.Deadline;
 import org.hibernate.search.engine.cfg.spi.ConfigurationPropertySource;
+import org.hibernate.search.mapper.orm.massindexing.MassIndexingFailureHandler;
 
-public class HibernateSearch61TestClass {
+public class HibernateSearch61TestClass implements org.hibernate.search.engine.backend.types.converter.FromDocumentFieldValueConverter, org.hibernate.search.engine.backend.types.converter.ToDocumentFieldValueConverter {
 
     public static void main(String[] args) {
         ConfigurationPropertySource configurationPropertySource;
@@ -23,6 +24,14 @@ public class HibernateSearch61TestClass {
         FieldPaths.absolutize("1", "2", "3");
         IndexManagerImplementor indexManagerImplementor;
         indexManagerImplementor.createIndexingPlan(null, null, null);
+        MassIndexingFailureHandler handler;
+        org.hibernate.search.mapper.orm.massindexing.MassIndexingMonitor monitor;
+
+        org.hibernate.search.mapper.orm.automaticindexing.AutomaticIndexingStrategyName strategyName;
+        String strat = org.hibernate.search.mapper.orm.cfg.HibernateOrmMapperSettings.AUTOMATIC_INDEXING_STRATEGY;
+        String strat2 = org.hibernate.search.mapper.orm.cfg.HibernateOrmMapperSettings.Radicals.AUTOMATIC_INDEXING_STRATEGY;
+        String strat3 = org.hibernate.search.mapper.orm.cfg.HibernateOrmMapperSettings.AutomaticIndexingRadicals.STRATEGY;
+        String strat4 = org.hibernate.search.mapper.orm.cfg.HibernateOrmMapperSettings.Defaults.AUTOMATIC_INDEXING_STRATEGY;
 
         IndexIndexingPlan indexIndexingPlan = null;
         indexIndexingPlan.executeAndReport();
