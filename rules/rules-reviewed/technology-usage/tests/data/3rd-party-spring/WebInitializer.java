c5513c6d9c3d19b8315159c7798d55be1a8b5c68
Zhiyong Li <iamlizhiyong@outlook.com>
2023-04-18 21:18:41 +0800

    use simple test data

---
+++ rules/rules-reviewed/technology-usage/tests/data/3rd-party-spring/WebInitializer.java
@@ -0,0 +1,9 @@
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class WebInitializer extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SimpleWebApplication.class);
+    }
+}
