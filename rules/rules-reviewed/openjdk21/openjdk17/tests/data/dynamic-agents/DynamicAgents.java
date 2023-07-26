291df58b01b6621d90c2577c6651381aaffc7f21
Juan Manuel Leflet Estrada <jleflete@redhat.com>
2023-07-26 22:35:50 +0200

    [WINDUPRULE-1016] Rule for dynamic loading of agents

---
+++ rules/rules-reviewed/openjdk21/openjdk17/tests/data/dynamic-agents/DynamicAgents.java
@@ -0,0 +1,7 @@
+import java.lang.instrument.ClassDefinition;
+
+public class DynamicAgents {
+    public static void main(String[] args) {
+        ClassDefinition classDefinition = new ClassDefinition(null, null);
+    }
+}
