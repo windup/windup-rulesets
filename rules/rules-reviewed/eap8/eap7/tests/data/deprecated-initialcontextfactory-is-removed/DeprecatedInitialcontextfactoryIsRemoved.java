3898ec3ab197b0523311ce7b01f7aeda82a1f00d
Eduardo Martins <emartins@redhat.com>
2023-12-11 16:07:48 +0000

    [WINDUPRULE-1019] adds eap 8 ruleset deprecated-initialcontextfactory-is-removed (#1044)

---
+++ rules/rules-reviewed/eap8/eap7/tests/data/deprecated-initialcontextfactory-is-removed/DeprecatedInitialcontextfactoryIsRemoved.java
@@ -0,0 +1,10 @@
+import org.jboss.naming.remote.client.InitialContextFactory;
+
+public class DeprecatedInitialcontextfactoryIsRemoved {
+
+    String propertyValue = "org.jboss.naming.remote.client.InitialContextFactory";
+    
+    public String getDeprecatedClassName() {
+        return InitialContextFactory.class.getName();
+    }
+}
