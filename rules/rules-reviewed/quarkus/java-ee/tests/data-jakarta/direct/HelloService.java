2e3e7035d0921faf7bdbd72956c704d960fb53bd
Mark Brophy <36955467+m-brophy@users.noreply.github.com>
2024-01-11 13:54:14 +0000

    Winduprule 1041 (#9)
    * cgi to quarkus rules for jakarta classes
    
    * remove failing groovy tests

--- rules/rules-reviewed/quarkus/java-ee/tests/data-jakarta/direct/HelloService.java
+++ rules/rules-reviewed/quarkus/java-ee/tests/data-jakarta/direct/HelloService.java
@@ -1,9 +1,8 @@
 package sample;
 
-import jakarta.enterprise.context.Dependent;
 import jakarta.enterprise.inject.Produces;
 
-@Dependent
+
 public class HelloService {
     @Produces
     String createHelloMessage(String name) {
